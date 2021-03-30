93751c37fad42e9e9cbc9368f129b3dc3afa5cab
Elmer <56842806+maycarlos@users.noreply.github.com>
2021-03-30 21:38:40 +0100

    'geração de password e definição de objetivos'

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Password generator
+
+Um básico gerador de passwords para treinar algumas coisas que fui aprendendo ao longo do tempo.
+Usa um módulo que tem que ser instalado previamente que é o [Alive Progress](https://github.com/rsalmei/alive-progress) de resto acho que é tudo built-in no python.
+
+## Mais umas coisinhas para fazer
+
+- [ ] Perguntar onde é que esta pass vai ser utilizada e adicionar esta informação no output da da função.  
+- [ ] Aprender e guardar as passwords num ficheiro JSON(ou outro tipo de ficheiro lindo onde se possa guardar informação) acompanhadas com o user, sítio onde é utilizada e claro a password encriptada(maybe?).  
+- [ ] Sim? ...aprender ou explorar diferentes tipos de encriptação básicos.
+- [ ] Criar um menu na command line para escolher o tipo de encriptação que desejo utilizar e aplica-la na pass antes de a guardar no ficheiro json.  
+- [ ] Fazer todo o processo reverso quando quiser consultar as passwords quardadas.  
+- [ ] Não? ... se calhar, começar a ver e trabalhar com databases.  
---
+++ pass_gen.py
@@ -0,0 +1,82 @@
+import random
+import string
+import json 
+import getpass
+import time
+
+#Precisa de ser instalada previamente -> pip install alive_progress
+from alive_progress import alive_bar
+
+#Não tenho a certeza se devo meter isto dentro da função chosen_password_parameters ou deixo aqui fora
+letras = string.ascii_letters
+punctuation = string.punctuation
+numero = string.digits
+
+#não se usa a string.printable() para dar a oportunidade de poder escolher o que deseja ter na pass
+
+def password_lenght():
+    return int(input('Insira o tamanho da password que deseja obter: '))
+
+def chosen_password_parameters():
+    """
+    Pedir ao utilizador os parametros que quer na sua password
+    Letras 
+    punctuation = 
+    numero = 0123456789
+    """
+    quer_letras = input('Queres letras na password?(True/False): ')
+    quer_puntuaction = input('Queres pontuação no password?(True/False): ')
+    quer_numeros = input('Queres números na password?(True/False): ')
+
+    #Percebi que não pode usar sets nem tuples, porque são immutable
+    resultado = [quer_letras, quer_puntuaction, quer_numeros]
+
+    #Substituir todos as strings com Booleans 
+    for i,j in enumerate(resultado):
+        resultado[i] = eval(j)
+
+    return resultado
+    
+def characters_included(escolha):
+    """
+    Dá nos uma string com o caracteres que desejamos que sejam incluídos na password.
+    Em função da lista que resulta da função chosen_password_parameters
+    """
+    modelo_da_pass = ''
+    modelo_da_pass += letras if escolha[0] else ''
+    modelo_da_pass += punctuation if escolha[1] else ''
+    modelo_da_pass += numero if escolha[2] else ''
+
+    return modelo_da_pass
+
+def generate_password(constituintes ,tamanho = 8):
+    """
+    Depois de termos todos os parametros da password definidos
+    Damos shufle a string com os constituintes e cortamos a string resultante em função do tamanho determinado
+    """
+    baralho = list(constituintes)
+    random.shuffle(baralho)
+
+    pass_final = ''.join(baralho)
+
+    #Aqui apenas pego nos primeiros 8 caracteres da string baralhada, se calhar devo escolher letras aleatoriamente da string(?)
+    return pass_final[:tamanho]
+
+def main():
+    tamanho = password_lenght()
+
+    parametros_desejados = chosen_password_parameters()
+    molde_password = characters_included(parametros_desejados)
+
+    password_desejada = generate_password(molde_password, tamanho)
+    
+    #Sem utilidade nenhuma, apenas acho engraçado ter barras em trabalhos 
+    with alive_bar(100, 'A gerar a password') as bar:
+        for _ in range(100):
+            time.sleep(0.01)
+            bar()
+
+    print(f'User:\t\t{getpass.getuser().title()}.\nPassword:\t{password_desejada}') 
+
+if __name__ == '__main__':
+    main()
